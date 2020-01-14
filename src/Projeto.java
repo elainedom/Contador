8f1fc42f86dec096cfd1826d8a042e356141e03c
senha-mastertech <Elaine Cardoso>
2020-01-14 15:44:53 -0300

    adicionar textos

--- src/Projeto.java
+++ src/Projeto.java
@@ -1,10 +1,12 @@
 public class Projeto {
     public static void main(String[] args) {
 
-        int contador = 0;
+        int contador = 1;
         while(contador <=10){
             System.out.println(contador);
             contador ++;
+
+            System.out.println("Contadorzinho barato");
         }
     }
 }
